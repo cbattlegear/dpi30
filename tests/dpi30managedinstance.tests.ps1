415c88309979910bcb22770f38e2bce1ba3acb1e
Cameron Battagler <cabattag@microsoft.com>
2020-01-09 10:34:57 -0600

    Basic tests for ARM Templates via Pester and Azure Pipelines

---
+++ tests/dpi30managedinstance.tests.ps1
@@ -0,0 +1,32 @@
+Param(
+  [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
+  [string] [Parameter(Mandatory=$true)] $TemplateFile,
+  [hashtable] [Parameter(Mandatory=$true)] $Parameters
+)
+
+Describe "DPi30 Managed Instance Deployment Tests" {
+  BeforeAll {
+    $DebugPreference = "Continue"
+  }
+
+  AfterAll {
+    $DebugPreference = "SilentlyContinue"
+  }
+
+  Context "When Managed Instance deployed with parameters" {
+    $output = Test-AzResourceGroupDeployment `
+              -ResourceGroupName $ResourceGroupName `
+              -TemplateFile $TemplateFile `
+              -TemplateParameterObject $Parameters `
+              -ErrorAction Stop `
+               5>&1
+               
+    $outstring = [string]::Concat($output[27])
+    $outjson = (($outstring -split "Body:")[1] | ConvertFrom-Json)
+    $result = $outjson.properties
+
+    It "Should be deployed successfully" {
+      $result.provisioningState | Should -Be "Succeeded"
+    }
+  }
+}
